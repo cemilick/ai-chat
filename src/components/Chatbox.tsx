485cd94272f866c8adedea416df4db44768fd0c5
luisperotto <luis@team-xyz.com>
2025-05-02 15:49:13 +0700

    init

--- src/components/Chatbox.tsx
+++ src/components/Chatbox.tsx
@@ -18,7 +18,7 @@ export default function ChatBox() {
     try {
         setLoading(true);
       const response = await axios.post(
-              `http://localhost:11434 /api/generate`,
+              `http://localhost:11434/api/generate`,
               {
                   model: "deepseek-r1",
                   prompt: newUserMessage.text,
